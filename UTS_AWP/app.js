3234d659a66206c32f546c16c3fc68380ebd9402
Gus Patin <mfaathinn@gmail.com>
2025-10-21 01:52:38 +0700

    Use environment variable for server port
    Updated server to use environment variable for PORT.

--- UTS_AWP/app.js
+++ UTS_AWP/app.js
@@ -41,5 +41,7 @@ app.use((req, res) => {
 });
 
 // Jalankan server
-const PORT = 3000;
-app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
