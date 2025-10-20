3d2787dc4dda7cd28b3f63aa6ad0f3c50acc508a
Gus Patin <mfaathinn@gmail.com>
2025-10-21 02:38:06 +0700

    Update connect.js

--- connect.js
+++ connect.js
@@ -1,36 +1,25 @@
-// connect.js
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "cs_kelompok5",
+    host: "id-dci-web1321.main-hosting.eu",
+    user: "u355073407_kelompok5",
+    password: "Kelompok5bijikopi.",
+    database: "u355073407_cs_kelompok5",
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
 });
 
-// export versi promise supaya bisa await
+
+// Cek koneksi untuk memastikan database bisa diakses
+pool.getConnection((err, connection) => {
+    if (err) {
+        console.error("❌ Database connection failed:", err.message);
+    } else {
+        console.log("✅ Database connected!");
+        connection.release(); // lepas koneksi kembali ke pool
+    }
+});
+
+// export versi promise supaya bisa pakai async/await
 module.exports = pool.promise();
-
-
-
-// connect.js
-// const mysql = require("mysql2");
-
-// // Buat pool koneksi
-// const pool = mysql.createPool({
-//     host: "mysql.hostinger.co.id",  // ganti sesuai info server Hostinger
-//     user: "u355073407_kelompok5",
-//     password: "Kelompok5bijikopi.",
-//     database: "u355073407_cs_kelompok5",
-//     waitForConnections: true,
-//     connectionLimit: 10,
-//     queueLimit: 0
-// });
-
-// // Export pool pakai promise supaya bisa await di route
-// module.exports = pool.promise();
-
-  
